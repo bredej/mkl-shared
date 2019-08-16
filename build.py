0a4672b14dd84f6921212b499d5f403ffb8710f3
Michael Gardner <shellshocked2003@gmail.com>
2019-08-16 08:30:02 -0700

    Changed builder type from bincrafters package tools

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ from bincrafters import build_template_installer
 
 if __name__ == "__main__":
 
-    builder = build_template_installer.get_builder()
+    builder = build_template_default.get_builder()
     builder.run()
