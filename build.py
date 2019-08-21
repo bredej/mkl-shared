dc38ec6142994030e587a6ee658df638fe04ae95
Michael Gardner <shellshocked2003@gmail.com>
2019-08-21 15:33:12 -0700

    Only build release and x86_64

--- build.py
+++ build.py
@@ -7,4 +7,6 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
+                options={}, env_vars={}, build_requires={})
     builder.run()
